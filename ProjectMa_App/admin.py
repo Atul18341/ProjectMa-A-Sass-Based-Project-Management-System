3ca35095c3ebb095e2a9af31cd514708dc062e8c
Surendra <surendrakakani5040@gmail.com>
2021-11-26 10:19:07 +0530

    saving

--- ProjectMa_App/admin.py
+++ ProjectMa_App/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Semester,MCE_CSE_Faculties,MCE_Student_Records
+from .models import Semester,MCE_CSE_Faculties,MCE_Student_Records,MCE_Student_Details,ReportDetails
 # Register your models here.
 admin.site.register(Semester)
 admin.site.register(MCE_CSE_Faculties)
 admin.site.register(MCE_Student_Records)
+admin.site.register(MCE_Student_Details)
+admin.site.register(ReportDetails)
